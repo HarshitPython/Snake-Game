95a5ff2520992c1febb282e958ba665676a0f5d5
HarshitPython <88823245+HarshitPython@users.noreply.github.com>
2021-09-02 15:05:19 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+# from turtle import Turtle, Screen
+# from snake import Snake
+# import time
+
+# screen = Screen()
+# screen.setup(width=600, height=600)
+# screen.bgcolor("black")
+# screen.title("Snack Game")
+# screen.tracer(0)
+
+# snake = Snake()
+
+# screen.listen()
+# screen.onkeypress(snake.up, "Up")
+# screen.onkeypress(snake.down, "Down")
+# screen.onkeypress(snake.right, "Right")
+# screen.onkeypress(snake.left, "Left")
+
+# game_is_on = True
+
+# while game_is_on:
+#     screen.update()
+#     time.sleep(0.1)
+
+#     snake.move()
+    
+
+# screen.exitonclick()
