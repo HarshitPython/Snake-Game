4d5d0448010a3ba05f22c46bbd641de03905b25a
HarshitPython <88823245+HarshitPython@users.noreply.github.com>
2021-09-03 19:29:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,6 +33,24 @@ while game_is_on:
     time.sleep(0.1)
 
     snake.move()
-    
+    #Detect collision with food
+    if snake.head.distance(food) < 20:
+        food.refresh()
+        snake.extend()
+        score.increase_score()
+        
+
+    #Detect collision with Wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -285:
+        game_is_on = False
+        score.game_over()
+
+    #Detect collision with tail.
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            score.game_over()
+   
+screen.exitonclick()
 
 screen.exitonclick()
